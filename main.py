307b202a85b5b9548efdf28f05d127ede3ea3c27
Roderick Burkhardt <rburkhardt@csumb.edu>
2016-04-02 20:33:41 -0700

    Adventure Strategy Game

---
+++ main.py
@@ -0,0 +1,17 @@
+'''
+
+  Program:  Name of Program
+  File name:  help.py
+  Files imported:  None
+  Author:  name here
+  Team:  MB Overloaders
+  Date created:  2016/04/02
+  Date last modified:  2016/04/02
+  Version:  0.0.1
+
+'''
+
+import help
+
+
+# Begin Code
